932dc4f5d71faea357a13d1987eaecb383b1ff28
decyple <markamccorkle@yahoo.com>
2023-02-10 09:53:14 -0600

    Initial Commit - 202302100933
    Added the GUI in a relatively close to final state.
    
    Added Visitor Class that will hold the data for the visitors that are currently in the MakerSpace and visitors that have visited for the day.
    
    Added the Class Class to hold the data for a given course that has been imported in from the current course catalog.
    
    Added the string array for the classes to hold the class detail read from the csv file. However, it will be later changed to hold the class class object. The data file was retrieved using a SQL query from student accounts. The SQL Query is called: NPCC_SCHED_DIV
    
    Added the string array for the orgs used to hold all of the different orgs on campus. The orgs are pulled directly from: https://np.edu/student-services/student-organizations.aspx
    
    Added the reasons enum that holds the different reasons a visitor would be using the space. This is hardcoded and was previously read in from a file. Since, this is unlikely to be changed, the categories were changed to being hardcoded.
    
    Added the ReasonToString function which converts the enum to a string name version.
    
    Added the ReadFiles function the reads the classes and orgs lists in from text files.
    
    Added the load_cbReason that loops through the Reasons, converts to a string, then adds them to the combobox.
    
    Added the search button function. It checks for an empty textbox, determines if the user is searching the class list or the orgs list, and adds the results to the lbClassList listbox. All while wrapped in a try statement to capture any errors.
    
    Added functionality to change the check-in/out button to reflect in or out status.
    
    Added the cbReason_SelectedIndexChanged function that clears the search results listbox and uses a switch statement to capture the switching of the combobox. Once, the switch is made logic is applied to determine what the forms state needs to be in for the particular Reason.
    
    Added the WriteData function that will be later written to write the data to various files.
    
    Added the DataValidation function that will later be sed to validate all of the data on the forms has been filled out correctly.
    
    Added the tbSearch_KeyUp function to allow the user to enter the enter key when searching the class/org lists instead of the search button.

---
+++ Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace NPC_MakerSpace_CheckIn
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.lblID = new System.Windows.Forms.Label();
+      this.tbID = new System.Windows.Forms.TextBox();
+      this.lblReason = new System.Windows.Forms.Label();
+      this.cbReason = new System.Windows.Forms.ComboBox();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.cbInOut = new System.Windows.Forms.CheckBox();
+      this.lblEstHours = new System.Windows.Forms.Label();
+      this.tbEstHours = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.gbSearch = new System.Windows.Forms.GroupBox();
+      this.lbClassList = new System.Windows.Forms.ListBox();
+      this.lblSearch = new System.Windows.Forms.Label();
+      this.tbSearch = new System.Windows.Forms.TextBox();
+      this.btnSearch = new System.Windows.Forms.Button();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.copyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+      this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+      this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.statusBar1 = new System.Windows.Forms.StatusBar();
+      this.btnComplete = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.gbSearch.SuspendLayout();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // lblID
+      // 
+      this.lblID.Location = new System.Drawing.Point(73, 35);
+      this.lblID.Name = "lblID";
+      this.lblID.Size = new System.Drawing.Size(144, 15);
+      this.lblID.TabIndex = 5;
+      this.lblID.Text = "ID:";
+      // 
+      // tbID
+      // 
+      this.tbID.Location = new System.Drawing.Point(73, 53);
+      this.tbID.Name = "tbID";
+      this.tbID.Size = new System.Drawing.Size(144, 20);
+      this.tbID.TabIndex = 0;
+      // 
+      // lblReason
+      // 
+      this.lblReason.Location = new System.Drawing.Point(223, 34);
+      this.lblReason.Name = "lblReason";
+      this.lblReason.Size = new System.Drawing.Size(188, 15);
+      this.lblReason.TabIndex = 6;
+      this.lblReason.Text = "Reason:";
+      // 
+      // cbReason
+      // 
+      this.cbReason.FormattingEnabled = true;
+      this.cbReason.Location = new System.Drawing.Point(223, 52);
+      this.cbReason.Name = "cbReason";
+      this.cbReason.Size = new System.Drawing.Size(188, 21);
+      this.cbReason.TabIndex = 1;
+      this.cbReason.SelectedIndexChanged += new System.EventHandler(this.cbReason_SelectedIndexChanged);
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBox1.TabIndex = 10;
+      this.pictureBox1.TabStop = false;
+      // 
+      // cbInOut
+      // 
+      this.cbInOut.Appearance = System.Windows.Forms.Appearance.Button;
+      this.cbInOut.Location = new System.Drawing.Point(481, 51);
+      this.cbInOut.Name = "cbInOut";
+      this.cbInOut.Size = new System.Drawing.Size(49, 21);
+      this.cbInOut.TabIndex = 3;
+      this.cbInOut.Text = "In/Out";
+      this.cbInOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.cbInOut.UseVisualStyleBackColor = true;
+      this.cbInOut.CheckedChanged += new System.EventHandler(this.cbInOut_CheckedChanged);
+      // 
+      // lblEstHours
+      // 
+      this.lblEstHours.Location = new System.Drawing.Point(417, 34);
+      this.lblEstHours.Name = "lblEstHours";
+      this.lblEstHours.Size = new System.Drawing.Size(58, 15);
+      this.lblEstHours.TabIndex = 13;
+      this.lblEstHours.Text = "Est Hours:";
+      // 
+      // tbEstHours
+      // 
+      this.tbEstHours.Location = new System.Drawing.Point(417, 52);
+      this.tbEstHours.Name = "tbEstHours";
+      this.tbEstHours.Size = new System.Drawing.Size(58, 20);
+      this.tbEstHours.TabIndex = 2;
+      // 
+      // label1
+      // 
+      this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.label1.Location = new System.Drawing.Point(12, 89);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(518, 2);
+      this.label1.TabIndex = 14;
+      // 
+      // gbSearch
+      // 
+      this.gbSearch.Controls.Add(this.lbClassList);
+      this.gbSearch.Controls.Add(this.lblSearch);
+      this.gbSearch.Controls.Add(this.tbSearch);
+      this.gbSearch.Controls.Add(this.btnSearch);
+      this.gbSearch.Enabled = false;
+      this.gbSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.gbSearch.Location = new System.Drawing.Point(12, 104);
+      this.gbSearch.Name = "gbSearch";
+      this.gbSearch.Padding = new System.Windows.Forms.Padding(0);
+      this.gbSearch.Size = new System.Drawing.Size(518, 185);
+      this.gbSearch.TabIndex = 15;
+      this.gbSearch.TabStop = false;
+      // 
+      // lbClassList
+      // 
+      this.lbClassList.FormattingEnabled = true;
+      this.lbClassList.Location = new System.Drawing.Point(30, 70);
+      this.lbClassList.Name = "lbClassList";
+      this.lbClassList.Size = new System.Drawing.Size(457, 95);
+      this.lbClassList.TabIndex = 6;
+      // 
+      // lblSearch
+      // 
+      this.lblSearch.Location = new System.Drawing.Point(30, 26);
+      this.lblSearch.Name = "lblSearch";
+      this.lblSearch.Size = new System.Drawing.Size(144, 15);
+      this.lblSearch.TabIndex = 6;
+      this.lblSearch.Text = "Search:";
+      // 
+      // tbSearch
+      // 
+      this.tbSearch.Location = new System.Drawing.Point(30, 44);
+      this.tbSearch.Name = "tbSearch";
+      this.tbSearch.Size = new System.Drawing.Size(379, 20);
+      this.tbSearch.TabIndex = 4;
+      this.tbSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbSearch_KeyUp);
+      // 
+      // btnSearch
+      // 
+      this.btnSearch.Location = new System.Drawing.Point(415, 44);
+      this.btnSearch.Name = "btnSearch";
+      this.btnSearch.Size = new System.Drawing.Size(72, 20);
+      this.btnSearch.TabIndex = 5;
+      this.btnSearch.Text = "Search";
+      this.btnSearch.UseVisualStyleBackColor = true;
+      this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.fileToolStripMenuItem, this.editToolStripMenuItem, this.helpToolStripMenuItem });
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(542, 24);
+      this.menuStrip1.TabIndex = 16;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.exitToolStripMenuItem });
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+      this.fileToolStripMenuItem.Text = "File";
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+      this.exitToolStripMenuItem.Text = "Exit";
+      this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+      // 
+      // editToolStripMenuItem
+      // 
+      this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.copyToolStripMenuItem, this.copyToolStripMenuItem1, this.pasteToolStripMenuItem, this.toolStripSeparator1, this.settingsToolStripMenuItem });
+      this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+      this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+      this.editToolStripMenuItem.Text = "Edit";
+      // 
+      // copyToolStripMenuItem
+      // 
+      this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+      this.copyToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+      this.copyToolStripMenuItem.Text = "Cut";
+      // 
+      // copyToolStripMenuItem1
+      // 
+      this.copyToolStripMenuItem1.Name = "copyToolStripMenuItem1";
+      this.copyToolStripMenuItem1.Size = new System.Drawing.Size(116, 22);
+      this.copyToolStripMenuItem1.Text = "Copy";
+      // 
+      // pasteToolStripMenuItem
+      // 
+      this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+      this.pasteToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+      this.pasteToolStripMenuItem.Text = "Paste";
+      // 
+      // toolStripSeparator1
+      // 
+      this.toolStripSeparator1.Name = "toolStripSeparator1";
+      this.toolStripSeparator1.Size = new System.Drawing.Size(113, 6);
+      // 
+      // settingsToolStripMenuItem
+      // 
+      this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+      this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+      this.settingsToolStripMenuItem.Text = "Settings";
+      // 
+      // helpToolStripMenuItem
+      // 
+      this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.aboutToolStripMenuItem });
+      this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+      this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+      this.helpToolStripMenuItem.Text = "Help";
+      // 
+      // aboutToolStripMenuItem
+      // 
+      this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+      this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+      this.aboutToolStripMenuItem.Text = "About";
+      // 
+      // statusBar1
+      // 
+      this.statusBar1.Location = new System.Drawing.Point(0, 349);
+      this.statusBar1.Name = "statusBar1";
+      this.statusBar1.Size = new System.Drawing.Size(542, 22);
+      this.statusBar1.TabIndex = 17;
+      this.statusBar1.Text = "statusBar1";
+      // 
+      // btnComplete
+      // 
+      this.btnComplete.Location = new System.Drawing.Point(12, 295);
+      this.btnComplete.Name = "btnComplete";
+      this.btnComplete.Size = new System.Drawing.Size(518, 37);
+      this.btnComplete.TabIndex = 18;
+      this.btnComplete.Text = "Complete";
+      this.btnComplete.UseVisualStyleBackColor = true;
+      this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(542, 371);
+      this.Controls.Add(this.btnComplete);
+      this.Controls.Add(this.statusBar1);
+      this.Controls.Add(this.gbSearch);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.lblEstHours);
+      this.Controls.Add(this.tbEstHours);
+      this.Controls.Add(this.cbInOut);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.cbReason);
+      this.Controls.Add(this.lblReason);
+      this.Controls.Add(this.lblID);
+      this.Controls.Add(this.tbID);
+      this.Controls.Add(this.menuStrip1);
+      this.Name = "Form1";
+      this.Text = "NPC MakerSpace Check-In";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.gbSearch.ResumeLayout(false);
+      this.gbSearch.PerformLayout();
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+    }
+
+    private System.Windows.Forms.Button btnComplete;
+
+    private System.Windows.Forms.StatusBar statusBar1;
+
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+
+    private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem1;
+    private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+
+    private System.Windows.Forms.GroupBox gbSearch;
+
+    private System.Windows.Forms.Label label1;
+
+    private System.Windows.Forms.Label lblEstHours;
+    private System.Windows.Forms.TextBox tbEstHours;
+
+    private System.Windows.Forms.CheckBox cbInOut;
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+
+    private System.Windows.Forms.ComboBox cbReason;
+
+    private System.Windows.Forms.Label lblID;
+    private System.Windows.Forms.TextBox tbID;
+    private System.Windows.Forms.Label lblReason;
+
+    private System.Windows.Forms.TextBox tbSearch;
+    private System.Windows.Forms.Label lblSearch;
+    private System.Windows.Forms.ListBox lbClassList;
+
+    private System.Windows.Forms.Button btnSearch;
+
+    #endregion
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NPC_MakerSpace_CheckIn
+{
+    class Visitor
+    {
+        
+    }
+
+    class Class
+    {
+        // AcadOrg,
+        // ClassNbr,
+        // CourseDescription,
+        // CourseID,
+        // Last,
+        // CourseType,
+        // MeetingDays,
+        // StartTime,
+        // EndTime,
+        // FacilID,
+        // StartDate,
+        // EndDate,
+        // EnrlStat,
+        // CapEnrl,
+        // TotEnrl,
+        // WaitTot,
+        // Session,
+        // Location,
+        // Mode
+        private String AcadOrg           = null;
+        private String ClassNbr          = null;
+        private String CourseDescription = null;
+        private String CourseID          = null;
+        private String Last              = null;
+        private String CourseType        = null;
+        private String MeetingDay        = null;
+        private String StartTime         = null;
+        private String EndTime           = null;
+        private String FacilID           = null;
+        private String StartDate         = null;
+        private String EndDate           = null;
+        private String EnrlStat          = null;
+        private String CapEnrl           = null;
+        private String TotEnrl           = null;
+        private String WaitTot           = null;
+        private String Session           = null;
+        private String Location          = null;
+        private String Mode              = null;
+    }
+    public partial class Form1 : Form
+    {
+        private string[] _classes;
+        private string[] _orgs;
+        enum Reasons : ushort
+        {
+            Null = 0,
+            Consultation = 1,
+            PersonalProject = 2,
+            Class = 3,
+            StudentOrgProject = 4,
+            Workshop = 5,
+            Commission = 6,
+            MAX = 7
+        }
+        
+        // Convert the enum to a string variable:
+        private string ReasonToString( int index )
+        {
+            switch (index)
+            {
+                case 0:
+                    return "";
+                case 1:
+                    return "Consultation";
+                case 2:
+                    return "Personal Project";
+                case 3:
+                    return "Class";
+                case 4:
+                    return "Student Org Project";
+                case 5:
+                    return "Workshop";
+                case 6:
+                    return "Commission";
+            }
+
+            return "";
+        }
+    
+        public Form1()
+        {
+            InitializeComponent();
+
+            ReadFiles();
+            
+            load_cbReason();
+        }
+
+        // Read the files for the classes and the orgs into global variables:
+        private void ReadFiles()
+        {
+            try
+            {
+                _classes = System.IO.File.ReadAllLines(@"C:\Users\decyple\class_list.dat");
+                _orgs    = System.IO.File.ReadAllLines(@"C:\Users\decyple\student_orgs.dat");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(@"Error reading files..." + Environment.NewLine + Environment.NewLine + e.Message);
+            }
+        }
+
+        private void load_classes()
+        {
+            
+        }
+
+        private void load_orgs()
+        {
+            
+        }
+        
+        // Loop through each of the reasons, convert them to a string, then add them to the check box:
+        private void load_cbReason()
+        {
+            for (int i = 0; i < (int)Reasons.MAX - 1; i++)
+            {
+                cbReason.Items.Add(ReasonToString(i));
+            }
+        }
+
+        // Add the btnSearch click functionality and also error proofing:
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(tbSearch.Text))
+                    MessageBox.Show(@"Please enter a search term...");
+                else
+                {
+                    string[] result = { "" };
+                    if (cbReason.SelectedIndex == (int)Reasons.Class)
+                    {
+                        // Query the class list using the search term and find all entries that match: 
+                        result = Array.FindAll(_classes, element => element.ToLower().Contains(tbSearch.Text.ToLower()));
+                    }
+                    else if (cbReason.SelectedIndex == (int)Reasons.StudentOrgProject)
+                    {
+                        // Query the orgs list using the search term and find all entries that match: 
+                        result = Array.FindAll(_orgs, element => element.ToLower().Contains(tbSearch.Text.ToLower()));
+                    }
+
+                    // Clear the list to start afresh:
+                    lbClassList.Items.Clear();
+
+                    // If we found classes, add them. Otherwise, notify the user no entries were found:
+                    if (result.Length != 0)
+                        lbClassList.Items.AddRange(result.ToArray<object>());
+                    else
+                        lbClassList.Items.Add(@"No results found...");
+                }
+            }
+            catch (ArgumentNullException e2)
+            {
+                MessageBox.Show(@"Error reading files..." + Environment.NewLine + Environment.NewLine + e2.Message);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cbInOut_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbInOut.Checked)
+                cbInOut.Text = @"In";
+            else
+                cbInOut.Text = @"Out";
+        }
+
+        // General form reset command:
+        private void ResetForm()
+        {
+            tbID.Text = "";
+            cbReason.SelectedIndex = (int)Reasons.Null;
+            tbEstHours.Text = "";
+            cbInOut.Checked = false;
+            cbInOut.Text = @"In/Out";
+            gbSearch.Visible = false;
+        }
+        private void cbReason_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear the list and search box to start afresh:
+            lbClassList.Items.Clear();
+            tbSearch.Clear();
+            
+            switch (cbReason.SelectedIndex)
+            {
+                case (int)Reasons.Null:                     // Blank
+                    // Search isn't needed for this reason:
+                    gbSearch.Enabled = false;
+
+                    break;                
+                case (int)Reasons.Consultation:             // Consultation
+                    // Search isn't needed for this reason:
+                    gbSearch.Enabled = false;
+
+                    // Set the default hours for this category:
+                    tbEstHours.Text = 2.ToString();
+                    
+                    break;
+                case (int)Reasons.PersonalProject:         // Personal Project
+                    // Search isn't needed for this reason:
+                    gbSearch.Enabled = false;
+
+                    // Set the default hours for this category:
+                    tbEstHours.Text = 2.ToString();
+                    break;
+                case (int)Reasons.Class:                    // Class
+                    // Enable the search functionality to search the class list:
+                    gbSearch.Enabled = true;
+                    
+                    // Set the default hours for this category:
+                    tbEstHours.Text = 1.ToString();
+
+                    break;
+                case (int)Reasons.StudentOrgProject:     // Student Org Project
+                    // Enable the search functionality to search the org list:
+                    gbSearch.Enabled = true;
+                    
+                    // Set the default hours for this category:
+                    tbEstHours.Text = 2.ToString();
+                    
+                    // No need to search a small list so directly inject the orgs:
+                    lbClassList.Items.AddRange(_orgs);
+                    
+                    break;
+                case (int)Reasons.Workshop:                 // Workshop
+                    // Search isn't needed for this reason:
+                    gbSearch.Enabled = false;
+                    
+                    // Set the default hours for this category:
+                    tbEstHours.Text = 4.ToString();
+                    
+                    break;
+                case (int)Reasons.Commission:               // Commission
+                    // Search isn't needed for this reason:
+                    gbSearch.Enabled = false;
+                    
+                    // Set the default hours for this category:
+                    tbEstHours.Text = 3.ToString();
+                    
+                    break;
+            }
+        }
+
+        // This function will write the data to a data file. Later there will be several files written. This includes a
+        // txt log file, excel spreadsheet, and others that have yet to be determined:
+        private void WriteData()
+        {
+            MessageBox.Show(@"[TODO] Write data...");
+        }
+
+        // This function will check several factors to verify the data enter is correct and in the form needed to be
+        // stored later:
+        private void DataValidation()
+        {
+            MessageBox.Show(@"[TODO] Validate data...");
+        }
+
+        // Finish by validating the forms fully filled-out, the data is written, and then reset:
+        private void btnComplete_Click(object sender, EventArgs e)
+        {
+            // Validate all fields are populated:
+            DataValidation();
+            
+            // Write the data to the file:
+            WriteData();
+            
+            // Reset the form to the default values:
+            ResetForm();
+        }
+
+        // This provides the same functionality as pressing the search button by entering the enter key:
+        private void tbSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)13)
+                btnSearch.PerformClick();
+        }
+    }
+}
